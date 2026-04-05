c3427468aa1c88d3c09b447b139aee423f999326
Yuxuan Li <yuxuanll@andrew.cmu.edu>
2026-04-05 12:37:10 -0400

    update

---
+++ talks.js
@@ -0,0 +1,32 @@
+const talks = [
+  {
+    title: "Pitfalls and Promise of Social Intelligence in Language Agents",
+    venues: [
+      { date: "Jan 2026", institution: "Tsinghua University", department: "Department of Psychological and Cognitive Sciences", logo: "materials/tsinghua_logo.png" }
+    ]
+  },
+  {
+    title: "Examine Machine Behavior and Simulate Society with Social Agents",
+    venues: [
+      { date: "Dec 2025", institution: "Microsoft Research Asia", department: "Social Computing Group", logo: "materials/msr_logo.svg" },
+      { date: "Dec 2025", institution: "Tsinghua University", department: "Pervasive HCI Group", logo: "materials/tsinghua_logo.png" },
+      { date: "Dec 2025", institution: "Tsinghua University", department: "School of Safety Science", logo: "materials/tsinghua_logo.png" },
+      { date: "Oct 2025", institution: "Georgia Institute of Technology", department: "Graph Computation and Machine Learning Lab", logo: "materials/gatech_logo.png" },
+      { date: "Oct 2025", institution: "Carnegie Mellon University", department: "Social Web Course", logo: "materials/cmu_logo.svg" },
+      { date: "Oct 2025", institution: "Carnegie Mellon University", department: "FEAT (Fairness, Ethics, Accountability, Transparency) AI Seminar", logo: "materials/cmu_logo.svg" }
+    ]
+  },
+  {
+    title: "Actions Speak Louder than Words: Agent Decisions Reveal Implicit Biases in Language Models",
+    venues: [
+      { date: "Dec 2025", institution: "The University of Hong Kong", department: "Faculty of Education", logo: "materials/hku_logo.png" },
+      { date: "Jun 2025", institution: "Microsoft Research", department: "alt.FAccT", logo: "materials/msr_logo.svg" }
+    ]
+  },
+  {
+    title: "A Practical Guide to Building Social Agents",
+    venues: [
+      { date: "Nov 2024", institution: "Carnegie Mellon University", department: "Social Agent Course", logo: "materials/cmu_logo.svg" }
+    ]
+  }
+];
